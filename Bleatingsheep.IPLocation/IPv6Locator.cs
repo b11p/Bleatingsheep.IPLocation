54498c19cd756c08480926f117ee2f96d5224b8a
bleatingsheep <bleatingsheep@live.cn>
2018-11-29 23:47:02 +0800

    0.2.0 增加识别原始地址的功能。

--- Bleatingsheep.IPLocation/IPv6Locator.cs
+++ Bleatingsheep.IPLocation/IPv6Locator.cs
@@ -12,8 +12,12 @@ namespace Bleatingsheep.IPLocation
 
         protected abstract Task<(bool, IIPLocation)> GetIPv6LocationAsync(IPAddress address);
 
+        /// <summary>
+        /// Gets original IP (if under known tunnel types).
+        /// </summary>
         /// <exception cref="ArgumentNullException"></exception>
-        public override async Task<(bool, IIPLocation)> GetLocationAsync(IPAddress address)
+        /// <returns>If <c>address</c> is an IPv6 Mapped Address, or 6to4 address, or Teredo address, the IPv4 it represents; otherwise, <c>address</c> itself.</returns>
+        public static IPAddress GetPureIP(IPAddress address)
         {
             if (address == null)
             {
@@ -22,22 +26,37 @@ namespace Bleatingsheep.IPLocation
 
             if (address.AddressFamily == AddressFamily.InterNetwork)
             {
-                return await IPv4FallbackLocator.GetLocationAsync(address);
+                return address;
             }
             if (address.IsIPv4MappedToIPv6)
             {
-                return await IPv4FallbackLocator.GetLocationAsync(address.MapToIPv4());
+                return address.MapToIPv4();
             }
             var bytes = address.GetAddressBytes();
             if (address.IsIPv6Teredo)
             {
-                return await IPv4FallbackLocator.GetLocationAsync(new IPAddress(bytes.Skip(12).Select(b => (byte)(255 - b)).ToArray()));
+                return new IPAddress(bytes.Skip(12).Select(b => (byte)(255 - b)).ToArray());
             }
             if (bytes[0] == 0x20 && bytes[1] == 0x02)
             {
-                return await IPv4FallbackLocator.GetLocationAsync(new IPAddress(bytes.Skip(2).Take(4).ToArray()));
+                return new IPAddress(bytes.Skip(2).Take(4).ToArray());
             }
-            return await GetIPv6LocationAsync(address);
+
+            return address;
+        }
+
+        /// <exception cref="ArgumentNullException"></exception>
+        public override async Task<(bool, IIPLocation)> GetLocationAsync(IPAddress address)
+        {
+            if (address == null)
+            {
+                throw new ArgumentNullException(nameof(address));
+            }
+
+            address = GetPureIP(address);
+            return address.AddressFamily == AddressFamily.InterNetwork
+                ? await IPv4FallbackLocator.GetLocationAsync(address)
+                : await GetIPv6LocationAsync(address);
         }
     }
 }
